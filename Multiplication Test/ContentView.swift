5ff165e2c1347be1e11afe5bfc9ff4e449819067
avan36 <158532304+avan36@users.noreply.github.com>
2024-06-24 01:08:24 +0100

    Initial Commit

---
+++ Multiplication Test/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Multiplication Test
+//
+//  Created by Ambrose V on 24/06/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Multiplication Test/Multiplication_TestApp.swift
@@ -0,0 +1,17 @@
+//
+//  Multiplication_TestApp.swift
+//  Multiplication Test
+//
+//  Created by Ambrose V on 24/06/2024.
+//
+
+import SwiftUI
+
+@main
+struct Multiplication_TestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
